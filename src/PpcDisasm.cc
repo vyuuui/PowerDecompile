cc5e3965faf86f43fcc63e2ebd286a9a161bd4d0
hzzzr <eltonha4@gmail.com>
2023-02-04 01:53:11 -0500

    change numbering

--- src/PpcDisasm.cc
+++ src/PpcDisasm.cc
@@ -15,9 +15,9 @@ void disasm_paired_single(BinInst binst, MetaInst& meta_out) {
 
 void disasm_single(BinInst binst, MetaInst& meta_out) {
   switch (binst._opcode) {
-    case 3:
+    case 1:
       break;
-    case 4:
+    case 2:
       disasm_paired_single(binst, meta_out);
       break;
   }
